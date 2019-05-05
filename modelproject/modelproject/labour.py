77abcbab62976f8e1354ec05bcf9fc86a82a507e
cth391 <47327645+cth391@users.noreply.github.com>
2019-05-05 13:25:37 +0200

    enter

--- modelproject/modelproject/labour.py
+++ modelproject/modelproject/labour.py
@@ -10,24 +10,34 @@ import scipy.integrate as integrate
 #Parameters to set: T available time, w function, A non-labour income
 
 ## Utility functions
-
+# test set parameters (CB)
+alpha=0.5
+tax2=0.5
+tax1=0.3
+tax0=0
+w=10
+cut1=4
+cut2=7
+T=10
+A=10
 # Cobb-Douglas:
-def  cobbdouglas(c,l):
+def  _cobbdouglas(c,l):
     """ Cobb douglas utility function for 2 goods.
     PARAMETERS
-    alpha: utility elasticity of consumption
+    alpha: income share spent on consumption
     INPUT:
     c: consumption
     l: leisure
 
     OUTPUT:
-    u: utility """
+    u: utility (negative)"""
     u= (c**alpha)*l**(1-alpha)
-    return u
+    return -u
+def  cobbdouglas(x):
+    return _cobbdouglas(c=x[0],l=x[1])
 
 # CES utility function:
-
-def CES(c,l):
+def _CES(c,l):
     """ CES utility function for 2 goods.
     PARAMETERS:
     r: 1/(1-r) is the elasticity of substitution, r<=1.
@@ -38,37 +48,75 @@ def CES(c,l):
     l: leisure (hrs pr. week)
 
     OUTPUT:
-    u: utility """
+    u: utility (neg) """
     u=(a*c**r + (1-a)*l**r)**(1/r)
-    return u
+    return -u
+def CES(x):
+    """ Takes a tuple as argument:
+    ARGS
+    x is a 2x1
+    x0= consumption
+    x1= leisure
+    OUTPUT
+    utility from consumption
+    """
+    return _CES(c=x[0],l=x[1]) 
 
-## Budget constraint
-# R0 is potential income if all time is spent
-# working, T the time available, and A
-# is non labour income
-#wage function 
+## Budget constraint (BC)
+# Three elements are defined for the (BC):
+#1) wage function: returning the after tax wagerate for
+# for a given level of leisure consumption (the more leisure
+#  the lower taxrate = higher wagerate).
+#2) maxwage: returns the wage earned given all time endowed is
+# spent working.
+#3) The budget function: calculates the slack in budget given
+#   choices of leisure and consumption (this is the contrained
+#   funtion).
+
+
+#Wage function 
 def wage(l):
-    if l < cut1:
-        return w0
-    elif cut1<= l and l <= cut2:
-        return w1
+    """ Wage function for a progressive tax system with allowing for two specified kinks
+        2 kinks (3 tax brackets).
+        
+        PARAMETERS:
+        cut1, cut2: cutoff for tax brackets defined by consumption of
+                    leisure.
+        w0,w1,w2: wage rates in the respective tax brackets.
+        
+        ARGS:
+        demand typle, x: x[0] cons, x[1] lesiure consumption
+        
+        OUTPUT:
+        w0,w1,w2: the appropriate marginal after tax wage rate
+    """
+    if l < cut2:
+        return w*(1-tax2)
+    elif cut2<= l and l <= cut1:
+        return w*(1-tax1)
     else:
-        return w2
+        return w*(1-tax0)
 
-maxwage = integrate.quad(wage, 0, T, args= (t1,t2))
-def budget(c,l, w, T,A):
-    R0=maxwage + A
-    slack= R0 - w*l + - C #w is wage rate for working, C and l are
-    return  slack         # consumption and lesiure respectively.
+# Return labour income if all time is spent working:
+maxwage = cut2*w*(1-tax2)+(cut1-cut2)*w*(1-tax1)+(T-cut1)*w*(1-tax0)
+# Budget constraint
+def budget(x):
+    c=x[0]
+    l=x[1]
+    R0= maxwage + A
+    slack= R0 - wage(l)*l - c #w is wage rate for working, c and l are
+    return  slack       # consumption and lesiure respectively.
 budget_con={'type':'eq', 'fun':budget}
-
-## Additional constrains / bounds:
-bounds= Bounds([0,np.inf], {0, np.inf})
-
-
+bounds= Bounds([0,np.inf], {0, T})
+## Optimization:
+guess=(5, 50)
+result = optimize.minimize(cobbdouglas,guess,
+                             method='SLSQP',
+                             constraints=[budget_con],
+                             options={'disp':True})
  
 
-
+print('\nx = ',result.x)
 
 
 
